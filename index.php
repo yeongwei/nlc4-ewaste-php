7a35dcce6f5cd5b6107dd476a36f580499b59631
vincebhleo <vincebhleo@gmail.com>
2017-02-20 14:06:49 +0800

    Initial updates

--- index.php
+++ index.php
@@ -1,21 +1,21 @@
 <!DOCTYPE html>
 <html>
-<head>
-	<title>PHP Starter Application</title>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-	<link rel="stylesheet" href="style.css" />
-</head>
-<body>
-	<table>
-		<tr>
-			<td style='width: 30%;'>
-				<img class = 'newappIcon' src='images/newapp-icon.png'>
-			</td>
-			<td>
-				<h1 id = "message"><?php echo "Hello World!"; ?></h1>
-				<p class='description'></p> Thanks for creating a <span class="blue">PHP Starter Application</span>.
-			</td>
-		</tr>
-	</table>
-</body>
+  <head>
+    <title>eWaste Management App</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <link rel="stylesheet" href="style.css" />
+  </head>
+  <body>
+    <table>
+      <tr>
+        <td style='width: 30%;'>
+          <img class = 'newappIcon' src='images/newapp-icon.png'>
+        </td>
+        <td>
+          <h1 id = "message"><?php echo "Welcome to eWaste Management App"; ?></h1>
+            <p class='description'></p> A better way to dispose <span class="blue">eWaste</span>.
+        </td>
+      </tr>
+    </table>
+  </body>
 </html>
