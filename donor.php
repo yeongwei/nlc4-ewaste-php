a05e69042775d08d5e8fcb5a7eea07276a06945a
vincebhleo <vincebhleo@gmail.com>
2017-02-25 21:18:51 +0800

    Add donor query

--- donor.php
+++ donor.php
@@ -9,6 +9,35 @@
     <link rel="shortcut icon" type="image/png" href="images/persistent-favicon.png"/>
     <title>eWaste Management App</title>
 </head>
+<?php
+// Read MySQL credentials from VCAP services and formatting
+$vcap_services = json_decode ( $_ENV ["VCAP_SERVICES"] );
+$db = $vcap_services->{"compose-for-mysql"} [0]->credentials;
+$temp = explode ( '@', $db->uri );
+$mysql_cred = explode ( ':', $temp [0] );
+$mysql_db = explode ( '/', $temp [1] );
+
+// Create DB connection
+$mysqli = new mysqli ( $mysql_db [0], ltrim ( $mysql_cred [1], "/" ), $mysql_cred [2], $mysql_db [1] );
+
+// Check DB connection
+if ($mysqli->connect_error) {
+    die ( "Connection failed: " . $mysqli->connect_error );
+}
+
+// To update insert query from form
+$sql = "select distinct city from ewaste_user where _role = 'volunteer'";
+$result = $mysqli->query($sql);
+
+if ($result->num_row > 0)
+    while($row = $result->fetch_assoc()) {
+        echo "city: " . $row["city"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$mysqli->close ();
+?>
 <body>
     <img src="images/BackgoundEcoEnvcrop.jpg" alt="BackgoundEcoEnvcrop">
     <h2 class="title">Find Your Nearest Collection Point</h2>
