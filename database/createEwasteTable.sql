19ecd9a9dd5b40e08cd15c721a1d55b153a87797
Ng Jiunn Jye <jiunnjye@gmail.com>
2017-02-25 11:38:27 +0800

    add sql scripts

---
+++ database/createEwasteTable.sql
@@ -0,0 +1,34 @@
+create table ewaste_user
+(
+   _id int AUTO_INCREMENT,
+   _index int,
+   guid varchar(255),
+   isactive boolean,
+   age int,
+   gender varchar(255),
+   name varchar(255),
+   company varchar(255),
+   _role varchar(255),
+   email varchar(255),
+   phone varchar(255),
+   address varchar(255),
+   city varchar(255),
+   postcode int,
+   state varchar(255),
+   registered timestamp,
+   latitude varchar(255),
+   longitude varchar(255),
+   primary key(_id)
+)
+
+create table ewaste_trx
+(
+   _id int AUTO_INCREMENT, -- transaction
+   donor_id int,
+   volunteer_id int,
+   recycler_id int, -- updated when recyler requested
+   weight double,
+   trx_date timestamp, 
+   status varchar(255), --A: available, R: Requested , C: Collected
+   primary key(_id) 
+)
