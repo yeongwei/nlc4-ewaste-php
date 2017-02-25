26afb919755dcc08b4bbf9cc56d49a250b888158
Ng Jiunn Jye <jiunnjye@gmail.com>
2017-02-26 03:19:06 +0800

    Update recycler flow

--- recycler-merchant-status.php
+++ recycler-merchant-status.php
@@ -22,14 +22,37 @@
         <h3 class="title">Merchant Weight Status</h3>
     </header>
     <div id="merchant-status">
-    <form action="action_page.php" id="recycler-city">
+    <form action="recycler-merchant-status.php" id="recycler-city">
         <div id="label_input">  
             <label for="city">City</label> 
             <select name="city" id="city" onchange="updateMerchantStats()">
-                <option value="kl">Kuala Lumpur</option>
-                <option value="penang">Penang</option>
-                <option value="pj">Petaling Jaya</option>
-                <option value="sunway">Sunway</option>
+<?php
+// Read MySQL credentials from VCAP services and formatting
+$vcap_services = json_decode ( $_ENV ["VCAP_SERVICES"] );
+$db = $vcap_services->{"compose-for-mysql"} [0]->credentials;
+$temp = explode ( '@', $db->uri );
+$mysql_cred = explode ( ':', $temp [0] );
+$mysql_db = explode ( '/', $temp [1] );
+
+// Create DB connection
+$mysqli = new mysqli ( $mysql_db [0], ltrim ( $mysql_cred [1], "/" ), $mysql_cred [2], $mysql_db [1] );
+
+// Check DB connection
+if ($mysqli->connect_error) {
+    die ( "Connection failed: " . $mysqli->connect_error );
+}
+
+// To update insert query from form
+$sql = "select distinct city from ewaste_user where _role = 'volunteer'";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        echo '<option value="' . $row['city']. '">' . $row['city']. '</option>';
+    }
+}
+$mysqli->close ();
+?>            
             </select>
         </div>
     </form>
@@ -41,21 +64,52 @@
         <tr>
             <th>Merchant</th><th>Current / max (kg)</th><th>Status (%)</th><th>Request</th>
         </tr>
-        <tr>
-            <td>
-                Auntie Anne's (1U)
-            </td>
-            <td>
-                3.25 / 5
-            </td>
-            <td>
-                65
-            </td>
-            <td>
-                collect
-            </td>
-        </tr>
-        <tr>
+<?php         
+$vcap_services = json_decode ( $_ENV ["VCAP_SERVICES"] );
+$db = $vcap_services->{"compose-for-mysql"} [0]->credentials;
+$temp = explode ( '@', $db->uri );
+$mysql_cred = explode ( ':', $temp [0] );
+$mysql_db = explode ( '/', $temp [1] );
+
+// Create DB connection
+$mysqli = new mysqli ( $mysql_db [0], ltrim ( $mysql_cred [1], "/" ), $mysql_cred [2], $mysql_db [1] );
+
+// Check DB connection
+if ($mysqli->connect_error) {
+    die ( "Connection failed: " . $mysqli->connect_error );
+}
+$city = $_GET['city'];
+// To update insert query from form
+$sql = "select usr.company as company, company_desc, image_path, sum_weight, sum_weight/50 * 100 as percent_full
+from 
+   (select volunteer_id, sum(weight) sum_weight from ewaste_trx 
+    where status = 'available'
+    group by volunteer_id) trx, ewaste_user usr
+where trx.volunteer_id = usr._id
+and usr.city = '" . $city . "'";
+$result = $mysqli->query($sql);
+
+if ($result->num_rows > 0) {
+    while($row = $result->fetch_assoc()) {
+        
+		echo '<tr>';
+        echo '    <td>';
+        echo $row['company'];
+        echo '    </td>';
+        echo '    <td>';
+        echo $row['sum_weight'] . ' / 50';
+        echo '    </td>';
+        echo '    <td>';
+        echo $row['percent_full'];
+        echo '    </td>';
+        echo '    <td>';
+        echo '        collect';
+        echo '    </td>';
+        echo '</tr>';
+    }
+}
+?>
+        <!-- tr>
             <td>
                 Baskin Robbins (1U)
             </td>
@@ -68,7 +122,7 @@
             <td>
                 collect
             </td>
-        </tr>
+        </tr-->
     </table>
     </div>
     <footer class="footer">
