7f40b87d08e40f56f824e73d2982cc2cb5b9a8be
YeongWei <ywlai@origineit.com>
2017-02-25 19:50:50 -0500

    Fixed recycler use friendly title

--- recycler.php
+++ recycler.php
@@ -1,6 +1,28 @@
 <?php
     include("config.php");
     include("classes/Helper.php");
+
+    $mysqli = null;
+    if (Helper::isDevelopment()) {
+        $mysqli = @new mysqli("10.211.50.18", "root", "Root#123", "console", "3306");
+    } else {
+        // Read MySQL credentials from VCAP services and formatting
+        $vcap_services = json_decode($_ENV ["VCAP_SERVICES"]);
+        $db = $vcap_services->{"compose-for-mysql"}[0]->credentials;
+        $temp = explode ("@", $db->uri);
+        $mysql_cred = explode (":", $temp [0]);
+        $mysql_db = explode ("/", $temp [1]);
+    
+        // Create DB connection
+        $mysqli = new mysqli ($mysql_db[0], ltrim($mysql_cred [1], "/"), $mysql_cred[2], $mysql_db[1]);
+    }
+
+    $_id = @$_GET["_id"];
+    $sql = "select usr.email as email, usr.phone as phone, usr.address as address, usr.city as city, usr.postcode as postcode,
+            usr.state as state, usr.latitude as latitude, usr.longitude as longitude, usr.company as company
+             from ewaste_user usr where usr._id = " . $_id;
+    $result = $mysqli->query($sql);
+    $rows = $result->fetch_assoc();
 ?>
 
 <!DOCTYPE html>
@@ -10,7 +32,6 @@
 
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>styles/style.css" />
     <link rel="stylesheet" href="<?php echo BASE_URL; ?>styles/recycler.css" />
-    <link rel="stylesheet" href="<?php echo BASE_URL; ?>styles/index.css" />
     <link rel="shortcut icon" type="image/png" href="<?php echo BASE_URL; ?>images/persistent-favicon.png"/>
     <title>eWaste Management App</title>
 </head>
@@ -19,7 +40,7 @@
     <div class="header-image">
         <img src="<?php echo BASE_URL; ?>images/BackgoundEcoEnvcrop.jpg" alt="BackgoundEcoEnvcrop">
     </div>
-    <div class="title-desc">Welcome Recycler</div>
+    <div class="title-desc">Welcome <?php echo $rows["company"]; ?></div>
     <br>
     <div class="recycler-welcome">       
         <a href="<?php echo BASE_URL; ?>collection.php" class="location-row">COLLECTION</a>
